7e6d4ca23375cb3a1610bc387eebbde4c1289a2d
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-11-13 01:09:34 -0500

    Revert "crash into a brick wall w/ mailin"
    This reverts commit 07ca57ba92856321c38ac0863e7c4515a56dd2d2.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use std::net::{IpAddr, SocketAddr};
-use std::sync::{Arc, Mutex};
-use anyhow::anyhow;
-
-use mailin::{Action, Handler};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -11,48 +6,33 @@ use log::{info, warn};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    let address = "127.0.0.1:8080";
-    let listener = TcpListener::bind(address).await?;
-    println!("Listening on: {}", address);
+    let addr = "127.0.0.1:8080";
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on: {}", addr);
 
     loop {
         // Asynchronously wait for an inbound socket.
-        let (socket, addr) = listener.accept().await?;
-        info!("client connected from {}", addr);
+        let (socket, a) = listener.accept().await?;
+        info!("client connected on {}", a);
 
         tokio::spawn(async move {
-            match handle_connection(socket, addr.ip()).await {
-                Ok(_) => info!("client disconnected cleanly from {}", addr),
-                Err(err) => warn!("broken client {}: {}", addr, err),
+            match handle_connection(socket).await {
+                Ok(_) => info!("client disconnected cleanly from {}", a),
+                Err(e) => warn!("broken client {}: {}", a, e),
             }
         });
     }
 }
 
-async fn handle_connection(mut socket: TcpStream, addr: IpAddr) -> anyhow::Result<()> {
+async fn handle_connection(mut socket: TcpStream) -> anyhow::Result<()> {
     let mut buf = vec![0; 1024];
-    // TODO: why isn't `Session: Send`?
-    let session = {
-        let s = mailin::SessionBuilder::new("worse-email").build(addr, MyHandler);
-        Arc::new(Mutex::new(s))
-    };
 
+    // In a loop, read data from the socket and write the data back.
     loop {
         let n = socket.read(&mut buf).await?;
         if n == 0 {
             return Ok(());
         }
-        let response = session.lock().unwrap().process(&buf);
-        match response.action {
-            Action::Close => return Ok(()),
-            Action::NoReply => continue,
-            Action::UpgradeTls => return Err(anyhow!("tls unsupported")),
-            Action::Reply => socket.write_all(&buf[0..n]).await?,
-        };
+        socket.write_all(&buf[0..n]).await?;
     }
-}
-
-struct MyHandler;
-impl Handler for MyHandler {
-
 }
