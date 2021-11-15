7c6a72eb698522d4291a6ed6832e089ef4090286
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-11-14 23:49:53 -0500

    do some state-tracking to support the DATA command

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use log::warn;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
 
 use crate::protocol::{parse_command, Command};
@@ -10,35 +10,57 @@ const AGENT: &str = "220 smtp.worse.email ESMTP Postfix\n";
 pub async fn handle_connection(mut socket: TcpStream) {
     if let Err(err) = connection_handler(&mut socket).await {
         warn!("broken client: {}", err);
-        let _ = socket.write_all(format!("error: {}", err).as_bytes()).await;
+        let _ = socket
+            .write_all(format!("error: {}\n", err).as_bytes())
+            .await;
     }
 }
-async fn connection_handler(socket: &mut TcpStream) -> anyhow::Result<()> {
-    socket.write_all(AGENT.as_bytes()).await?;
-    let mut buf = vec![0; 1024];
 
-    // In a loop, read data from the socket and write the data back.
+enum SessionState {
+    Initial,
+    ReadingData { data: Vec<u8> },
+}
+async fn connection_handler(socket: &mut TcpStream) -> anyhow::Result<()> {
+    let mut socket = BufReader::new(socket);
+    socket.write_all(AGENT.as_bytes()).await?;
+    let mut buf = Vec::with_capacity(1_024);
+
+    let mut state = SessionState::Initial;
     loop {
-        let n = socket.read(&mut buf).await?;
-        if n == 0 {
+        buf.clear();
+        socket.read_until(b'\n', &mut buf).await?;
+        if buf.is_empty() {
             return Ok(());
         }
-        let cmd = parse_command(&buf[..n])?;
-        println!("[RECV] got {:?}", cmd);
-        let reply = match cmd {
-            Command::Helo { domain: name } => {
-                format!("250 Hello {}, I am glad to meet you\n", name)
+        match state {
+            SessionState::ReadingData { ref mut data } => {
+                if buf == b".\n" || buf == b".\r\n" {
+                    socket.write_all("250 Ok\n".as_bytes()).await?;
+                    state = SessionState::Initial;
+                } else {
+                    data.extend(&buf);
+                }
             }
-            Command::Ehlo { domain: name } => {
-                format!("250 Hello {}, I am glad to meet you\n", name)
+            SessionState::Initial => {
+                let cmd = parse_command(&buf)?;
+                let reply = match cmd {
+                    Command::Helo { domain } => {
+                        format!("250 Hello {}, I am glad to meet you\n", domain)
+                    }
+                    Command::Ehlo { domain } => {
+                        format!("250 Hello {}, I am glad to meet you\n", domain)
+                    }
+                    Command::MailFrom { .. } => "250 Ok\n".to_owned(),
+                    Command::RcptTo { .. } => "250 Ok\n".to_owned(),
+                    Command::Data => {
+                        state = SessionState::ReadingData { data: Vec::new() };
+                        "354 End data with <CR><LF>.<CR><LF>\n".to_owned()
+                    }
+                    Command::Quit => return Ok(()),
+                };
+                socket.write_all(reply.as_bytes()).await?;
             }
-            Command::MailFrom { .. } => "250 Ok\n".to_owned(),
-            Command::RcptTo { .. } => "250 Ok\n".to_owned(),
-            Command::Data => "354 End data with <CR><LF>.<CR><LF>\n".to_owned(),
-            Command::Quit => return Ok(()),
         };
-        println!("[RECV] replying w/ {}", reply);
-        socket.write_all(reply.as_bytes()).await?;
     }
 }
 
@@ -47,7 +69,7 @@ mod test {
     use std::sync::atomic::AtomicU32;
 
     use tokio::{
-        io::{AsyncReadExt, AsyncWriteExt},
+        io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
         net::{TcpListener, TcpStream},
     };
 
@@ -55,69 +77,68 @@ mod test {
 
     // This is a little janky, we consume a port per test.
     static TEST_PORT: AtomicU32 = AtomicU32::new(1984);
-    async fn setup() -> anyhow::Result<TcpStream> {
+    async fn setup() -> anyhow::Result<BufReader<TcpStream>> {
         let port = TEST_PORT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         let addr = format!("localhost:{}", port);
         let listener = TcpListener::bind(&addr).await?;
         let client = TcpStream::connect(&addr).await?;
         let (socket, _) = listener.accept().await?;
         tokio::spawn(handle_connection(socket));
-        Ok(client)
+        Ok(BufReader::new(client))
     }
 
     #[tokio::test]
     async fn smoke_test() -> anyhow::Result<()> {
         let mut client = setup().await?;
-        let mut buf: Vec<u8> = vec![0; 1_024];
+        let mut buf = String::with_capacity(1_024);
 
-        let n = client.read(&mut buf).await?;
-        assert_eq!(String::from_utf8_lossy(&buf[..n]), AGENT);
+        client.read_line(&mut buf).await?;
+        assert_eq!(buf, AGENT);
+        buf.clear();
 
-        client.write_all("HELO example.com".as_bytes()).await?;
-        let n = client.read(&mut buf).await?;
-        assert_eq!(
-            String::from_utf8_lossy(&buf[..n]),
-            "250 Hello example.com, I am glad to meet you\n"
-        );
+        client.write_all("HELO example.com\n".as_bytes()).await?;
+        client.read_line(&mut buf).await?;
+        assert_eq!(buf, "250 Hello example.com, I am glad to meet you\n");
+        buf.clear();
         Ok(())
     }
 
     #[tokio::test]
     async fn quit_test() -> anyhow::Result<()> {
         let mut client = setup().await?;
-        client.write_all("QUIT".as_bytes()).await?;
+        client.write_all("QUIT\n".as_bytes()).await?;
 
-        let mut buf: Vec<u8> = vec![0; 1_024];
+        let mut buf = String::new();
         // The server should close the stream, so we should get back an empty read eventually.
-        while client.read(&mut buf).await? > 0 {}
+        while client.read_line(&mut buf).await? > 0 {}
         Ok(())
     }
 
     #[tokio::test]
     async fn data_test() -> anyhow::Result<()> {
         let mut client = setup().await?;
-        let mut buf: Vec<u8> = vec![0; 1_024];
+        let mut buf = String::new();
 
-        let n = client.read(&mut buf).await?;
-        assert_eq!(String::from_utf8_lossy(&buf[..n]), AGENT);
+        client.read_line(&mut buf).await?;
+        assert_eq!(buf, AGENT);
+        buf.clear();
 
-        client.write_all("DATA".as_bytes()).await?;
-        let n = client.read(&mut buf).await?;
-        assert_eq!(
-            String::from_utf8_lossy(&buf[..n]),
-            "354 End data with <CR><LF>.<CR><LF>\n"
-        );
+        client.write_all("DATA\n".as_bytes()).await?;
+        client.read_line(&mut buf).await?;
+        assert_eq!(buf, "354 End data with <CR><LF>.<CR><LF>\n");
+        buf.clear();
 
         // TODO(rpb): this should be legal, we need to do state-tracking in the handler.
         client.write_all("Line 1\n".as_bytes()).await?;
         client.write_all("Line 2\n".as_bytes()).await?;
         client.write_all("Line 3\n".as_bytes()).await?;
         client.write_all(".\n".as_bytes()).await?;
-        let n = client.read(&mut buf).await?;
-        assert_eq!(String::from_utf8_lossy(&buf[..n]), "250 Ok");
+        client.read_line(&mut buf).await?;
+        assert_eq!(buf, "250 Ok\n");
+        buf.clear();
 
-        client.write_all("QUIT".as_bytes()).await?;
-        while client.read(&mut buf).await? > 0 {}
+        client.write_all("QUIT\n".as_bytes()).await?;
+        while client.read_line(&mut buf).await? > 0 {}
         Ok(())
     }
 }
